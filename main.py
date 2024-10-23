718555165d17542778f1d47c4c81b4476df78c55
shephbra000 <shephbra000@warren.k12.in.us>
2024-10-23 07:56:14 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+card1 = int(input("Enter the value of the first card: 10"))
+
+card2 = int(input("Enter the value of the second card: 9"))
+
+card_sum = card1 + card2
+
+is_21_or_over = card_sum >= 21
+
+print("Is the sum 21 or over?", is_21_or_over)
